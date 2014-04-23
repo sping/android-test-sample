a9f84b31667f3f65fc10c6e4e7ec5ab818ab01e1
Glenn Vlekke <g.vlekke@sping.nl>
2014-04-23 14:33:48 +0200

    first test

---
+++ app/src/test/java/nl/sping/futuricetest/app/test/RoboTest.java
@@ -0,0 +1,15 @@
+package nl.sping.futuricetest.app.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest {
+    @Test
+    public void testTrueIsTrue() throws Exception {
+        assertEquals(true, false);
+    }
+}
