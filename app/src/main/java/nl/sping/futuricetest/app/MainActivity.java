9903d7345e7c087c07bb2385843d51c0695d011a
Glenn Vlekke <g.vlekke@sping.nl>
2014-05-16 16:36:47 +0200

    create real test

--- app/src/main/java/nl/sping/futuricetest/app/MainActivity.java
+++ app/src/main/java/nl/sping/futuricetest/app/MainActivity.java
@@ -1,17 +1,28 @@
 package nl.sping.futuricetest.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button button;
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textview = (TextView) findViewById(R.id.text);
+
+        button.setOnClickListener(this);
     }
 
 
@@ -35,4 +46,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        textview.setText("Testing Android Rocks!");
+    }
 }
--- app/src/test/java/nl/sping/futuricetest/app/test/RoboTest.java
+++ app/src/test/java/nl/sping/futuricetest/app/test/RoboTest.java
@@ -1,15 +1,35 @@
 package nl.sping.futuricetest.app.test;
 
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.TextView;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.*;
+import nl.sping.futuricetest.app.MainActivity;
+import nl.sping.futuricetest.app.R;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class RoboTest {
     @Test
     public void testTrueIsTrue() throws Exception {
-        assertEquals(true, false);
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void testActivityTextChange() {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        Button pressMeButton = (Button) activity.findViewById(R.id.button);
+        TextView results = (TextView) activity.findViewById(R.id.text);
+
+        pressMeButton.performClick();
+        String resultsText = results.getText().toString();
+        assertEquals(resultsText, "Testing Android Rocks!");
     }
 }
